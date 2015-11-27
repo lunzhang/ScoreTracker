223cef776be9b4e09faa739a419600ba94b0d000
lunzhang <breakermonkey55@yahoo.com>
2015-11-27 18:24:26 -0500

    edit game modal

--- www/js/controllers/gameController.js
+++ www/js/controllers/gameController.js
@@ -1,13 +1,5 @@
 scoretracker.controller('gameController', function($scope,$scoretracker,$localStorage,$ionicModal) {
 
   $scope.currentGame = $scoretracker.currentGame;
-  var modal = jQuery("#edit-modal").andSelf().html();
-  $scope.modal = $ionicModal.fromTemplate(modal, {
-      scope: $scope,
-      animation: 'slide-in-up'
-  });
 
-  $scope.editGame = function() {
-     $scope.modal.show();
-   };
 });
--- www/js/services/scoretracker.js
+++ www/js/services/scoretracker.js
@@ -3,17 +3,23 @@ scoretracker.service('$scoretracker', function($localStorage) {
   this.getGames = function(){
     return $localStorage.games;
   }
-
+  if(!$localStorage.games){
+    $localStorage.games= {};
+  }
   this.addGame = function(){
+    var nextId = parseInt( Object.keys($localStorage.games)[Object.keys($localStorage.games).length-1]  ) + 1;
+    if(!nextId){
+      nextId=0;
+    }
     var newGame = {
-      id: Object.keys($localStorage.games).length,
-      title : "Game " + Object.keys($localStorage.games).length,
-      player1 : "",
-      player2 : "",
+      id: nextId,
+      title : "Game " + nextId,
+      player1 : "Player 1",
+      player2 : "Player 2",
       player1Score : 0,
       player2Score : 0
     };
-    $localStorage.games[Object.keys($localStorage.games).length]=newGame;
+    $localStorage.games[nextId]=newGame;
   }
 
   this.deleteGame = function(game){
